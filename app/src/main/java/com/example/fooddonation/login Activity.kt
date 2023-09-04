5e0804ff5a7acebc0b205aa5de40367e4603dac5
Desokii <81819045+AbdelrahmanDesOki@users.noreply.github.com>
2023-09-04 17:58:34 +0200

    log in and sign up page basic design

--- app/src/main/java/com/example/fooddonation/login Activity.kt
+++ app/src/main/java/com/example/fooddonation/login Activity.kt
@@ -6,11 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.SemanticsProperties.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -24,12 +30,19 @@ fun App() {
     }
 }
 
+@Preview
 @Composable
 fun LoginPage(onSignUpClick: () -> Unit) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isButtonEnabled by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Green)
+            .background(Color.Gray)
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -39,6 +52,48 @@ fun LoginPage(onSignUpClick: () -> Unit) {
         // ... (rest of your login components)
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+                isButtonEnabled = it.isNotEmpty() && password.isNotEmpty()
+            },
+            label = { Text("Email", color = Color.White) },
+            keyboardOptions = KeyboardOptions(
+                imeAction = androidx.compose.ui.text.input.ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager.clearFocus()
+                }
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+                isButtonEnabled = it.isNotEmpty() && email.isNotEmpty()
+            },
+            label = { Text("Password", color = Color.White) },
+            keyboardOptions = KeyboardOptions(
+                imeAction = androidx.compose.ui.text.input.ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                }
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
 
         Button(
             onClick = { /* Handle login here */ },
@@ -61,10 +116,14 @@ fun LoginPage(onSignUpClick: () -> Unit) {
 
 @Composable
 fun SignUpPage(onLoginClick: () -> Unit) {
+    var name_sign_up by remember { mutableStateOf("") }
+    var email_sign_up by remember { mutableStateOf("") }
+    var password_sign_up by remember { mutableStateOf("") }
+    val focusManager_up = LocalFocusManager.current
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Green)
+            .background(Color.Gray)
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -74,6 +133,68 @@ fun SignUpPage(onLoginClick: () -> Unit) {
         // ... (rest of your sign-up components)
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = name_sign_up,
+            onValueChange = {
+                name_sign_up = it
+                email_sign_up.isNotEmpty() && password_sign_up.isNotEmpty()
+            },
+            label = { Text("Name", color = Color.White) },
+            keyboardOptions = KeyboardOptions(
+                imeAction = androidx.compose.ui.text.input.ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager_up.clearFocus()
+                }
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email_sign_up,
+            onValueChange = {
+                email_sign_up = it
+                name_sign_up.isNotEmpty() && password_sign_up.isNotEmpty()
+            },
+            label = { Text("Email", color = Color.White) },
+            keyboardOptions = KeyboardOptions(
+                imeAction = androidx.compose.ui.text.input.ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager_up.clearFocus()
+                }
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password_sign_up,
+            onValueChange = {
+                password_sign_up = it
+                email_sign_up.isNotEmpty() && name_sign_up.isNotEmpty()
+            },
+            label = { Text("Password", color = Color.White) },
+            keyboardOptions = KeyboardOptions(
+                imeAction = androidx.compose.ui.text.input.ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager_up.clearFocus()
+                }
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
 
         Button(
             onClick = { /* Handle sign-up here */ },
